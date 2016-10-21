c73e68d8faa25318bce41989b4df827cb4fff03b
Emily Turner-Sherwood <emilyturner@Emilys-MacBook-Pro.local>
2016-10-21 16:24:16 -0500

    fixed counter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,10 @@
 // Variables //
-
 var words = ['gorilla', 'leaf', 'ocean']; // Creating the array of words for the computer to choose from
 var currentWord = words[0];
 var dashes = [];
 var userGuesses = [];
 var guessedLetters = [];
-var matchedLetters; 
+var matchedLetters;
 var remainingGuesses = 12;
 var wins = 0;
 var correctLetter;
@@ -13,60 +12,61 @@ var wordSplit = []; //Splitting up the words in the array by letter to match use
 
 
 // Start of game //
-function startReset(){
+function startReset() {
 
     // Computer choosing the random word from array 
     currentWord = words[Math.floor(Math.random() * words.length)];
     console.log(currentWord); //Console.logs the hidden word for my reference
- }// End of startReset function
-    // Loop to show wordLength as underscores
-    for (var i = 0; i < currentWord.length; i++) {
-        dashes.push('_'); //.push will push whatever is in the ('') into the currentWord var
-    }
-    document.getElementById('currentWord').innerHTML = (dashes.join(' '));
-   
+} // End of startReset function
+// Loop to show wordLength as underscores
+for (var i = 0; i < currentWord.length; i++) {
+    dashes.push('_'); //.push will push whatever is in the ('') into the currentWord var
+}
+document.getElementById('currentWord').innerHTML = (dashes.join(' '));
 
-	//Will split the currentWord into single character strings
-    wordSplit = currentWord.split(''); 
-    console.log(wordSplit)
+//Will split the currentWord into single character strings
+wordSplit = currentWord.split('');
+console.log(wordSplit)
 
-    // Displaying initial set-up information
-    document.getElementById('winning').innerHTML = 'Wins: '
-    document.getElementById('guessesLeft').innerHTML = 'Number of Guesses Remaining: ' + remainingGuesses;
-    document.getElementById('alreadyGuessed').innerHTML = 'Letters Already Guessed: '
+// Displaying initial set-up information
+document.getElementById('winning').innerHTML = 'Wins: '
+document.getElementById('guessesLeft').innerHTML = 'Number of Guesses Remaining: ' + remainingGuesses;
+document.getElementById('alreadyGuessed').innerHTML = 'Letters Already Guessed: '
 
 // Game will begin when any key is pressed
 document.onkeyup = function(event) {
     userGuess = (String.fromCharCode(event.keyCode).toLowerCase());
 
-//checking to see if userGuess is in the word
-        	userGuesses.push(userGuess.toLowerCase());
-        	var matchedLetters = ''; // These are the correct letters the the user guesses
-        	for (i = 0; i<currentWord.length; i++) {
-        		if (userGuesses.indexOf(currentWord[i].toLowerCase()) > -1) {
-        		matchedLetters += currentWord[i];
-        	}
-        	else { 
-        		matchedLetters += '_ ';
-        		remainingGuesses--;
-        	}
-        	document.getElementById('guessesLeft').innerHTML = 'Number of Guesses Remaining: ' + remainingGuesses;
-        	document.getElementById('alreadyGuessed').innerHTML = 'Letters already Guessed: ' + userGuesses.join(', ');
+    //checking to see if userGuess is in the word
+    userGuesses.push(userGuess.toLowerCase());
+    var matchedLetters = ''; // These are the correct letters the the user guesses
+    for (i = 0; i < currentWord.length; i++) {
+        if (userGuesses.indexOf(currentWord[i].toLowerCase()) > -1) {
+            matchedLetters += currentWord[i];
+        } else {
+            matchedLetters += '_ ';
+        }
+        document.getElementById('guessesLeft').innerHTML = 'Number of Guesses Remaining: ' + remainingGuesses;
+        document.getElementById('alreadyGuessed').innerHTML = 'Letters already Guessed: ' + userGuesses.join(', ');
     }
+     	remainingGuesses--;
+
     console.log(matchedLetters);
     document.getElementById('currentWord').innerHTML = matchedLetters;
 
-if (currentWord == matchedLetters) {
-	alert ('You win!');
-	wins ++;
-	document.getElementById('winning').innerHTML = 'Wins: ' + wins;
-	startReset();
-}
-	else if  
-		(remainingGuesses <= 0) 
-	 		alert ('You lose! The word was ' + currentWord);
-	 		startReset();
+    if (currentWord == matchedLetters) {
+        alert('You win!');
+        wins++;
+        document.getElementById('winning').innerHTML = 'Wins: ' + wins;
+        startReset();
+    } else if (remainingGuesses <= 0) {
+        alert('You lose! The word was ' + currentWord);
+        startReset();
+    }
+
 };
+
+
 
 
 
